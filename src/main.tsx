8f427ad1ec5d2d429ae08d3ef928a19a89743ff8
Gianluca Sampogna <gianlucascalzisampogna@gmail.com>
2026-04-05 15:30:23 -0300

    config (react-router-dom): implementa a lib de páginas

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { router } from "./routes";
-import { RouterProvider } from "react-router/dom";
-import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "../Views/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home />,
   },
 ]);
 
