94b60b4db3cf7cf0c0f1a1db206f5f02ae1b563f
Gianluca Sampogna <gianlucascalzisampogna@gmail.com>
2026-04-05 22:07:45 -0300

    feat (sublinhado): implementa componente "sublinhado"

---
+++ src/components/Sublinhado/index.tsx
@@ -0,0 +1,12 @@
+import "./styles/index.css";
+
+const Sublinhado = () => {
+  return (
+    <div className="sublinhado">
+      <div id="bola" />
+      <div id="traco" />
+    </div>
+  );
+};
+
+export { Sublinhado };
